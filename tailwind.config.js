2091cfc7423410a6165d0e9fd263150d6eb3d861
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-06-13 19:23:04 -0300

    Update Files

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       colors: {
         'col-DER': 'hsl(0, 36%, 70%)',
         'col-SR': 'hsl(0, 93%, 68%)',
-        'col-DAR': 'hsl(0, 6%, 24%)'
+        'col-DAR': 'hsl(0, 6%, 24%)',
       },
       fontFamily: {
         'JS':'Josefin Sans'
@@ -14,6 +14,7 @@ module.exports = {
       backgroundImage: {
         'Linear1': 'linear-gradient(135deg, hsl(0, 0%, 100%), hsl(0, 100%, 98%))',
         'Linear2': 'linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))',
+        'Linear3': 'linear-gradient(135deg, #F9D2D3, #F8CECF)',
         'bg-Desk': "url('/images/bg-pattern-desktop.svg')",
         'Img-Mob': "url('/images/hero-mobile.jpg')",
         'Img-Desk': "url('/images/hero-desktop.jpg')"
