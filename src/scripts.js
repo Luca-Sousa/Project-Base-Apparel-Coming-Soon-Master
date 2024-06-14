1b7d1a09481d270cdd27c3fe8340a429527d5656
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-06-14 18:24:35 -0300

    Update Finally

--- src/scripts.js
+++ src/scripts.js
@@ -1,55 +1,56 @@
 $(function(){
 
     $('#email').focus(function(){
+        if ($('#messageEmpty').is(':visible') || $('#messageError').is(':visible')) {
+            $('#messageEmpty').hide();
+            $('#messageError').hide();
+        }
         resetInvalidField($(this));
     });
 
-    // Função para lidar com o clique no botão de submit
     $('form').submit(function(event){
-        // Evitar o envio padrão do formulário
         event.preventDefault();
 
         var email = $('input[name=email]').val();
-        console.log('email');
         var isValid = true;
 
-        // Verificação de Email
         if (email === '') {
             applyFieldInvalid($('input[name=email]'));
-            $('#message').append('<p>Please provide a valid email</p>');
+            $('#messageEmpty').show();
             isValid = false;
         } else if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).test(email)) {
             applyFieldInvalid($('input[name=email]'));
-            $('#message').append('<br><p>Please provide a valid email</p>');
+            $('#messageError').show();
             isValid = false;
         }
 
         if (isValid) {
-            alert("enviado com sucesso");
+            $('#messageSuccess').removeClass('hidden');
+            $('#messageSuccess').addClass('flex animate-move-in');
+
+            setTimeout(function() {
+                $('#messageSuccess').removeClass('animate-move-in');
+                $('#messageSuccess').addClass('animate-move-out');
+                $('input[name=email]').val('');
+
+                setTimeout(function() {
+                    $('#messageSuccess').removeClass('animate-move-out');
+                    $('#messageSuccess').removeClass('flex');
+                    $('#messageSuccess').addClass('hidden');
+                }, 500);
+            }, 7000);
         }
     });
 
-    // Função para aplicar estilo de campo inválido
     function applyFieldInvalid(el){
-        el.css('border', '1px solid col-Soft-Red');
+        el.removeClass('border border-gray-200');
+        el.addClass('border-2 border-col-SR');
     }
 
-    // Função para resetar estilo de campo inválido
     function resetInvalidField(el){
-        el.css('border', '1px solid col-Des-Red');
+        el.removeClass('border-2 border-col-SR');
+        el.addClass('border border-gray-200');
         el.val('');
-    }
-
-    // Função para resetar os estilos dos inputs de rádio e checkbox
-    function restartInputsCheckBox() {
-        $(".query-type-wraper").css({
-            'border': '1px solid var(--Medium--Grey)',
-            'background-color': 'var(--white)'
-        });
-        $(".radio-mark").css("display", "none");
-        $("input[type=radio]").css("display", "block");
-        $(".checkbox-mark").css("display", "none");
-        $("input[type=checkbox]").css("display", "block");
     }
     
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/*.css"],
+  content: ["./index.html", "./src/*.{css,js}"],
   theme: {
     extend: {
       colors: {
@@ -15,18 +15,36 @@ module.exports = {
         'Linear1': 'linear-gradient(135deg, hsl(0, 0%, 100%), hsl(0, 100%, 98%))',
         'Linear2': 'linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))',
         'Linear3': 'linear-gradient(135deg, #F9D2D3, #F8CECF)',
-        'bg-Desk': "url('/images/bg-pattern-desktop.svg')",
-        'Img-Mob': "url('/images/hero-mobile.jpg')",
-        'Img-Desk': "url('/images/hero-desktop.jpg')"
+        'bg-Desk': "url('/src/assets/images/bg-pattern-desktop.svg')",
+        'Img-Mob': "url('/src/assets/images/hero-mobile.jpg')",
+        'Img-Desk': "url('/src/assets/images/hero-desktop.jpg')"
       },
       backgroundSize: {
         '100-100': '100% 100%'
       },
       screens: {
         lxl: '1600px'
-      }
+      },
+      animation: {
+        'move-in': 'move-in 1.5s forwards',
+        'move-out': 'move-out 4s forwards',
+      },
+      keyframes: {
+        'move-in': {
+          'from': { top: '-100vh' },
+          'to': { top: '75px' },
+        },
+        'move-out': {
+          'from': { top: '75px' },
+          'to': { top: '-100vh' },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      animation: ['hover', 'focus'],
     },
   },
   plugins: [],
 }
-
